2789bb96ba4a807f2170b89e57e36965cae9ad17
JeffH-AWS <jeffhuss@amazon.com>
2023-03-21 08:49:02 -0700

    Stashing changes
    Signed-off-by: JeffH-AWS <jeffhuss@amazon.com>

--- data-prepper.sh
+++ data-prepper.sh
@@ -1,12 +1,23 @@
 #! /bin/bash
 
-# Start the Fluent Bit container
-init_fluent_bit() {
-    docker run -d \
-        -v fluent-bit.conf:/fluent-bit/etc/fluent-bit.conf -v 
-}
-
 # Write the default config for Fluent Bit
 write_fluent_bit_conf()	{
-	cat <<- 'EOF' > fluent-bit.conf
+    echo "Writing Fluent Bit config file..."
+	cat <<- ''EOF' > fluent-bit.conf
 }
+
+# Create test log file
+init_test_log() {
+    echo "Initializing test log file..."
+    touch test.log
+}
+
+# Start the Fluent Bit container
+init_fluent_bit() {
+    echo "Launching Fluent Bit container..."
+    docker run -d \
+        -v fluent-bit.conf:/fluent-bit/etc/fluent-bit.conf \
+        -v test.log:/var/log/test.log \
+        --network opensearch-net
+}
+
