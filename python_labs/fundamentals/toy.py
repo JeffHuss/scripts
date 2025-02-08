e26dabd53e8c909a7ec402026bf948175c957d60
Jeff H <jeff.huss@comcast.net>
2025-02-08 15:54:08 -0800

    Adding exercise that wasn' t saved previously

--- python_labs/fundamentals/toy.py
+++ python_labs/fundamentals/toy.py
@@ -1,9 +1,13 @@
+# Define class object for "robot_dog"
 class robot_dog:
     def __init__(self, name_val, breed_val):
+        # add name and breed
         self.name = name_val
         self.breed = breed_val
     def bark(self):
+        # Barks
         print('Woof woof!')
+
 
 my_dog = robot_dog('Finn', 'Chihuahua')
 
