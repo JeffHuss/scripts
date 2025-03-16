243c0aae431ba67db1becc02c36bb591e9c12ca8
Jeff H <jeff.huss@comcast.net>
2025-03-16 15:44:37 -0700

    Moved variables to external file variables.tf

--- terraform/learn-terraform-docker-container/main.tf
+++ terraform/learn-terraform-docker-container/main.tf
@@ -11,27 +11,6 @@ provider "docker" {
   host = "unix:///home/jhuss/.docker/desktop/docker.sock"
 }
 
-variable "docker_ports" {
-  type = list(object({
-    internal = number
-    external = number
-    protocol = string
-  }))
-  description = "Internal and external ports with protocol for Docker containers"
-  default = [
-  { # [0] HTTP
-    internal = 80
-    external = 8080
-    protocol = "tcp"
-  },
-  { # [1] HTTPS
-    internal = 443
-    external = 8443
-    protocol = "tcp"
-  }
-]
-}
-
 resource "docker_image" "nginx" {
   name         = "nginx:latest"
   keep_locally = false
