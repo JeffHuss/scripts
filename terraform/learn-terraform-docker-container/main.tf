45ce22d2b04bfe111f0f4e6f9565c41c419a6d26
Jeff H <jeff.huss@comcast.net>
2025-03-16 15:35:58 -0700

    Added default http and https ports and generated a plan

--- terraform/learn-terraform-docker-container/main.tf
+++ terraform/learn-terraform-docker-container/main.tf
@@ -11,6 +11,27 @@ provider "docker" {
   host = "unix:///home/jhuss/.docker/desktop/docker.sock"
 }
 
+variable "docker_ports" {
+  type = list(object({
+    internal = number
+    external = number
+    protocol = string
+  }))
+  description = "Internal and external ports with protocol for Docker containers"
+  default = [
+  { # [0] HTTP
+    internal = 80
+    external = 8080
+    protocol = "tcp"
+  },
+  { # [1] HTTPS
+    internal = 443
+    external = 8443
+    protocol = "tcp"
+  }
+]
+}
+
 resource "docker_image" "nginx" {
   name         = "nginx:latest"
   keep_locally = false
@@ -20,7 +41,8 @@ resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
   name  = "tutorial"
   ports {
-    internal = 80
-    external = 8000
+    internal = var.docker_ports[0].internal
+    external = var.docker_ports[0].external
+    protocol = var.docker_ports[0].protocol
   }
 }
